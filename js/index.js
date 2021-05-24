d3f5754adba1815b23a517b0d4405666207314f2
miluska schaeffer <miluska.schaeffer@gmail.com>
2021-05-24 16:30:42 +0200

    linking & navbar functionality

--- js/index.js
+++ js/index.js
@@ -10,3 +10,18 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+
+var text = ["FRONT-END DEV","UX/UI DESIGN"];
+var counter = 0;
+var elem = document.getElementById("changeText");
+var inst = setInterval(change, 2000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
+  }
+}
